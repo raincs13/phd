6fe6297b90822d1a016e8a82a05938a48afce3ad
panita <raincs13@hotmail.com>
2016-04-21 18:47:21 +0700

    Refactoring IPO GP =  [title, legend]

--- source/org/jfree/chart/ChartFactory.java
+++ source/org/jfree/chart/ChartFactory.java
@@ -2252,28 +2252,25 @@ public abstract class ChartFactory {
 
     /**
      * Creates and returns a default instance of a high-low-open-close chart.
-     *
-     * @param title  the chart title (<code>null</code> permitted).
+     * @param parameterObject TODO
      * @param timeAxisLabel  a label for the time axis (<code>null</code>
      *                       permitted).
      * @param valueAxisLabel  a label for the value axis (<code>null</code>
      *                        permitted).
      * @param dataset  the dataset for the chart (<code>null</code> permitted).
-     * @param legend  a flag specifying whether or not a legend is required.
      *
      * @return A high-low-open-close chart.
      */
-    public static JFreeChart createHighLowChart(String title,
-            String timeAxisLabel, String valueAxisLabel, OHLCDataset dataset,
-            boolean legend) {
+    public static JFreeChart createHighLowChart(CreateHighLowChartParameter parameterObject,
+            String timeAxisLabel, String valueAxisLabel, OHLCDataset dataset) {
 
         ValueAxis timeAxis = new DateAxis(timeAxisLabel);
         NumberAxis valueAxis = new NumberAxis(valueAxisLabel);
         HighLowRenderer renderer = new HighLowRenderer();
         renderer.setBaseToolTipGenerator(new HighLowItemLabelGenerator());
         XYPlot plot = new XYPlot(dataset, timeAxis, valueAxis, renderer);
-        JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT,
-                plot, legend);
+        JFreeChart chart = new JFreeChart(parameterObject.getTitle(), JFreeChart.DEFAULT_TITLE_FONT,
+                plot, parameterObject.isLegend());
         currentTheme.apply(chart);
         return chart;
 
