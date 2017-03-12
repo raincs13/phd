1f82806a6b2a9e2d7ddb92fd291af112a007bc4b
STD-2PANITA\Panita <raincs13@gmail.com>
2017-03-12 11:24:39 +0700

    EM Var = plot - B1

--- source/org/jfree/chart/ChartPanel.java
+++ source/org/jfree/chart/ChartPanel.java
@@ -2381,21 +2381,25 @@ public class ChartPanel extends JPanel implements ChartChangeListener,
      * Restores the auto-range calculation on the domain axis.
      */
     public void restoreAutoDomainBounds() {
-        Plot plot = this.chart.getPlot();
-        if (plot instanceof Zoomable) {
+        Plot plot = plot();
+		if (plot instanceof Zoomable) {
             Zoomable z = (Zoomable) plot;
-            // here we tweak the notify flag on the plot so that only
-            // one notification happens even though we update multiple
-            // axes...
-            boolean savedNotify = plot.isNotify();
-            plot.setNotify(false);
             // we need to guard against this.zoomPoint being null
             Point2D zp = (this.zoomPoint != null
                     ? this.zoomPoint : new Point());
             z.zoomDomainAxes(0.0, this.info.getPlotInfo(), zp);
-            plot.setNotify(savedNotify);
         }
     }
+
+	private Plot plot() {
+		Plot plot = this.chart.getPlot();
+		if (plot instanceof Zoomable) {
+			boolean savedNotify = plot.isNotify();
+			plot.setNotify(false);
+			plot.setNotify(savedNotify);
+		}
+		return plot;
+	}
 
     /**
      * Restores the auto-range calculation on the range axis.
