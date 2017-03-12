18d89b29ba0653514e12dbf09838cee2fd059412
STD-2PANITA\Panita <raincs13@gmail.com>
2017-03-12 12:46:12 +0700

    EM Var = plot - B2

--- source/org/jfree/chart/ChartPanel.java
+++ source/org/jfree/chart/ChartPanel.java
@@ -2404,18 +2404,19 @@ public class ChartPanel extends JPanel implements ChartChangeListener,
         Plot plot = this.chart.getPlot();
         if (plot instanceof Zoomable) {
             Zoomable z = (Zoomable) plot;
-            // here we tweak the notify flag on the plot so that only
-            // one notification happens even though we update multiple
-            // axes...
-            boolean savedNotify = plot.isNotify();
-            plot.setNotify(false);
-            // we need to guard against this.zoomPoint being null
+            plot(plot);
+			// we need to guard against this.zoomPoint being null
             Point2D zp = (this.zoomPoint != null
                     ? this.zoomPoint : new Point());
             z.zoomRangeAxes(0.0, this.info.getPlotInfo(), zp);
-            plot.setNotify(savedNotify);
         }
     }
+
+	private void plot(Plot plot) {
+		boolean savedNotify = plot.isNotify();
+		plot.setNotify(false);
+		plot.setNotify(savedNotify);
+	}
 
     /**
      * Returns the data area for the chart (the area inside the axes) with the
